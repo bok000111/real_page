6ef05c707a81caffeb649b1c732a39d30491733f
jbok <jbok@student.42seoul.kr>
2023-05-29 18:06:27 +0900

    close #1 /register 페이지 구현

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,142 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootState } from '@/reducers';
+import { setUser } from '@/reducers/authReducer';
+
+type Setter<T> = (param: T) => void;
+
+export default function Register() {
+  const [errors, setErrors] = useState<[string, [string]][]>([]);
+  const user = useSelector((state: RootState) => state.auth.user);
+  const dispatch = useDispatch();
+
+  function formSubmitHandler(data: {}) {
+    const url = 'https://api.realworld.io/api/users';
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+    console.log(data);
+    axios
+      .post(url, data, {
+        headers: headers,
+      })
+      .then((response) => {
+        dispatch(setUser(response.data.user));
+      })
+      .catch((error) => {
+        if (error.response) {
+          setErrors(Object.entries(error.response.data.errors));
+        } else {
+          setErrors([['Connection', ['is unstable']]]);
+        }
+      });
+  }
+
+  return (
+    <div className='auth-page'>
+      <div className='container page'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3 col-xs-12'>
+            <h1 className='text-xs-center'>Sign up</h1>
+            <p className='text-xs-center'>
+              <Link href='/login'>Have an account?</Link>
+            </p>
+            <TakenError errors={errors} />
+            <SignInForm handler={formSubmitHandler} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TakenError({ errors }: { errors: [string, [string]][] }) {
+  return (
+    <div>
+      {errors.map((error, i) => {
+        return (
+          <div key={i}>
+            {error[1].map((msg, i) => {
+              return (
+                <ul className='error-messages' key={i}>
+                  <li>{`${error[0]} ${msg}`}</li>
+                </ul>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function SignInForm({ handler }: { handler: (data: {}) => void }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+
+  return (
+    <form>
+      <FormTextInput placeholder='Your Name' setter={setName} />
+      <FormTextInput placeholder='Email' setter={setEmail} />
+      <FormPassInput placeholder='Password' setter={setPass} />
+      <button
+        type='submit'
+        className='btn btn-lg btn-primary pull-xs-right'
+        onClick={(e) => {
+          e.preventDefault();
+          handler({
+            user: {
+              username: name,
+              email: email,
+              password: pass,
+            },
+          });
+        }}>
+        Sign up
+      </button>
+    </form>
+  );
+}
+
+function FormTextInput({
+  placeholder,
+  setter,
+}: {
+  placeholder: string;
+  setter: Setter<string>;
+}) {
+  return (
+    <fieldset className='form-group'>
+      <input
+        className='form-control form-control-lg'
+        type='text'
+        placeholder={placeholder}
+        onChange={(e) => setter(e.target.value)}
+      />
+    </fieldset>
+  );
+}
+
+function FormPassInput({
+  placeholder,
+  setter,
+}: {
+  placeholder: string;
+  setter: Setter<string>;
+}) {
+  return (
+    <fieldset className='form-group'>
+      <input
+        className='form-control form-control-lg'
+        type='password'
+        placeholder={placeholder}
+        onChange={(e) => setter(e.target.value)}
+      />
+    </fieldset>
+  );
+}
